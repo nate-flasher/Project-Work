7bcd34dcb8c51844cdd64a83dfa15d3266e75f6e
Nate Flasher <nate_flasher@taylor.edu>
2019-04-25 12:23:29 -0400

    end of second code together

---
+++ course/cos109/lab/catalog/catalog.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+menutext = """
+C) Create new course
+R) Show records in course table
+U) Update a course
+D) Delete a course
+X) Exit the program
+"""
+
+
+connection = sqlite3.connect("catalog.sqlite")
+cursor = connection.cursor()
+
+
+def create_course():
+
+    print("\nCreate a new course")
+
+    new_code = input(">>> Course code: ")
+    new_title = input(">>> Course title: ")
+    new_credit = input(">>> Course credit hours: ")
+
+    values = (new_code, new_title, new_credit)
+
+    cursor.execute('INSERT INTO course (code, title, credit) VALUES (?, ?, ?) values')
+    connection.commit()
+
+
+def read_course():
+
+    print("\nCourse records:")
+    for row in cursor.execute('SELECT * FROM course'):
+        print(row)
+
+
+
+while True:
+
+    print(menutext)
+    selection = input(">>> C,R,U,D, or X ot exit")
+
+    if selection == 'X':
+        break
+
+    elif selection == 'C':
+        create_course()
+
+    elif selection == 'R':
+        read_course()
