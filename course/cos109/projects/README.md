e2cac5e5ac46f9da533f9e5ba0b8d1eab8608e30
Nate Flasher <nate_flasher@taylor.edu>
2019-05-07 16:55:51 -0400

    Done for the day...have everything but join and export done.

---
+++ course/cos109/projects/README.md
@@ -0,0 +1,15 @@
+# Contact and Group Database 
+
+## Description
+    -   This interactive database is designed to allow the user to manage 
+    their contacts and the groups in which their contacts are stored in a 
+    dynamic, interactive way. The database allows for creation, searches,
+    updates, deletes, and exports.
+    
+## Instructions
+    -   When wanting to access what is in the table (visually see the database),
+    double click on either the contact or group table in the right column. When
+    wanting to interact with the database double click on either the contact.py
+    or the group.py in the left column, then click the play button in the top
+    right. At this point a box will appear at the bottom in which you an follow 
+    the instructions to see how you may edit the database.
--- course/cos109/projects/contact.py
+++ course/cos109/projects/contact.py
@@ -1,10 +1,12 @@
 import sqlite3
+import csv
 
 menutext = """
 C) Create a contact
 R) Show records in contact table
 U) Update a contact
 D) Delete a contact
+E) Export data ot csv file
 X) Exit the program
 """
 
@@ -29,7 +31,8 @@ def create_contact():
 
     values = (new_firstname, new_lastname, new_emailaddress, new_secondaryemailaddress, new_organization, new_phone, new_secondaryphone, new_birthday, new_notes)
 
-    cursor.execute('INSERT INTO "contact" (Firstname, Lastname, "Email address", "Secondary email address", Organization, Phone, "Secondary phone", Birthday, Notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', values)
+    cursor.execute(
+        'INSERT INTO "contact" (Firstname, Lastname, Email_address, Secondary_email_address, Organization, Phone, Secondary_phone, Birthday, Notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', values)
     connection.commit()
 
 
@@ -41,23 +44,51 @@ def read_contact():
 
 
 def update_contact():
-    read_group()
-    modify_this_id = input(">>> Modify group (Which group number?): ")
+    read_contact()
+    modify_this_id = input(">>> Modify contact (Which contact number?): ")
 
-    cursor.execute("SELECT * FROM 'group' WHERE Group_number = ?", (modify_this_id))
-    (cur_name, cur_note, Group_number) = cursor.fetchone()
+    cursor.execute("SELECT * FROM 'contact' WHERE Contact_number = ?", (modify_this_id))
+    (cur_firstname, cur_lastname, cur_emailaddress, cur_secondaryemailaddress, cur_organization, cur_phone, cur_secondaryphone, cur_birthday, cur_notes, Contact_number) = cursor.fetchone()
 
-    new_note = input(">>> Group notes [%s]: " % cur_note)
-    if new_note == '':
-        new_note = cur_note
+    new_firstname = input(">>> Contact Firstname [%s]: " % cur_firstname)
+    if new_firstname == '':
+        new_firstname = cur_firstname
 
-    new_name = input(">>> Group name [%s]: " % cur_name)
-    if new_name == '':
-        new_name = cur_name
+    new_lastname = input(">>> Contact Lastname [%s]: " % cur_lastname)
+    if new_lastname == '':
+        new_lastname = cur_lastname
+
+    new_emailaddress = input(">>> Contact email address [%s]: " % cur_emailaddress)
+    if new_emailaddress == '':
+        new_emailaddress = cur_emailaddress
+
+    new_secondaryemailaddress = input(">>> Contact secondary email address [%s]: " % cur_secondaryemailaddress)
+    if new_secondaryemailaddress == '':
+        new_secondaryemailaddress = cur_secondaryemailaddress
+
+    new_organization = input(">>> Contact organization [%s]: " % cur_organization)
+    if new_organization == '':
+        new_organization = cur_organization
+
+    new_phone = input(">>> Contact phone number [%s]: " % cur_phone)
+    if new_phone == '':
+        new_phone = cur_phone
+
+    new_secondaryphone = input(">>> Contact secondary phone number [%s]: " % cur_secondaryphone)
+    if new_secondaryphone == '':
+        new_secondaryphone = cur_secondaryphone
+
+    new_birthday = input(">>> Contact birthday [%s]: " % cur_birthday)
+    if new_birthday == '':
+        new_birthday = cur_birthday
+
+    new_notes = input(">>> Contact notes [%s]: " % cur_notes)
+    if new_notes == '':
+        new_notes = cur_notes
 
 
-    values = (new_note, new_name, modify_this_id)
-    cursor.execute("UPDATE 'group' SET Notes = ?, Group_name = ? WHERE Group_number = ?", values)
+    values = (new_firstname, new_lastname, new_emailaddress, new_secondaryemailaddress, new_organization, new_phone, new_secondaryphone, new_birthday, new_notes, modify_this_id)
+    cursor.execute("UPDATE 'contact' SET Firstname = ?, Lastname = ?, Email_address = ?, Secondary_email_address = ?, Organization = ?, Phone = ?, Secondary_phone = ?, Birthday = ?, Notes = ? WHERE Contact_number = ?", values)
     connection.commit()
 
 
@@ -71,6 +102,11 @@ def delete_contact():
     connection.commit()
 
 
+def export_contact():
+
+    with open('addressBook.csv', 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow
 
 while True:
 
@@ -91,3 +127,6 @@ while True:
 
     elif selection == 'D':
         delete_contact()
+
+    elif selection == 'E':
+        export_contact()
