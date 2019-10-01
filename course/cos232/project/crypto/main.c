f97b932b1a8cf166e361e9f81240308aa63eb4af
nflasher <nate_flasher@taylor.edu>
2019-09-30 22:34:04 -0400

    Made huge progress! Got the looping mechanism implemented! Super happy!

--- course/cos232/project/crypto/main.c
+++ course/cos232/project/crypto/main.c
@@ -2,44 +2,80 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-char encrypt(char *typeOfChrypt, int key, int text){
+//fprintf( stderr, ...)
+char encrypt(char *typeOfChrypt, int key, int text) {
     if (text >= 65 && text <= 90) {
 
-        if (strcmp(typeOfChrypt , "-e") == 0) {
-            text = text + key;
-            return (char) text;
+
+        if (strcmp(typeOfChrypt, "-e") == 0) {
+            if ((text + key) > 90) {
+                text = 64 + key;
+                printf("%c", text);
+                return (char) text;
+            } else {
+                text = text + key;
+                printf("%c", text);
+                return (char) text;
+            }
         } else {
-            text = text - key;
-            return (char) text;
+            if ((text - key) < 65) {
+                text = 90 - key;
+                printf("%c", text);
+                return (char) text;
+            } else {
+                text = text - key;
+                printf("%c", text);
+                return (char) text;
+            }
         }
-    } else if (text >= 97 && text <= 122) {
+    }
 
+    else if (text >= 97 && text <= 122) {
 
-        if (strcmp(typeOfChrypt , "-e") == 0) {
-            text = text + key;
-            return (char) text;
-        } else {
-            text = text - key;
-            return (char) text;
+            if (strcmp(typeOfChrypt, "-e") == 0) {
+                if((text + key) > 122) {
+                    text = 96 + key;
+                    printf("%c", text);
+                    return (char) text;
+                }else {
+                    text = text + key;
+                    printf("%c", text);
+                    return (char) text;
+                }
+            } else {
+                if((text - key) < 97) {
+                    text = 122 - key;
+                    printf("%c", text);
+                    return (char) text;
+                }else {
+                    text = text - key;
+                    printf("%c", text);
+                    return (char) text;
+                }
+            }
         }
 
-
-
-    } else {
+    else {
         return (char) text;
     }
 }
 
 
+
+
 int main(int argc, char *argv[]) {
     char *key = argv[2];
     int convertedKey = atoi(key);
+
+    int modulusKey = convertedKey%26;
+
     char temp[strlen(argv[3])];
 
+
+
     for( int i=0; i < strlen(argv[3]); i++){
-        temp[i] = encrypt(argv[1], convertedKey, argv[3][i]);
-        printf("%s\n%s\n", &argv[3][i], temp);
+        temp[i] = encrypt(argv[1], modulusKey, argv[3][i]);
+        //printf("%s\n%s\n", &argv[3][i], temp);
     }
     return 0;
 }
