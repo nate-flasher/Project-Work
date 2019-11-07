58c3a6b556457e5f078f5f62b7041772d7d67a0e
Dannie M. Stanley <dannie.stanley@gmail.com>
2019-11-07 16:14:30 -0500

    Initial files for web project

---
+++ course/cos232/project/web/src/members.php
@@ -0,0 +1,83 @@
+<?php
+	// Connects to the Database 
+	include('connect.php');
+	$DB = connect();
+	
+	//if the login form is submitted 
+	if (isset($_POST['submit'])) {
+		
+		$_POST['username'] = trim($_POST['username']);
+		if(!$_POST['username'] | !$_POST['password']) {
+			die('<p>You did not fill in a required field.
+			Please go back and try again!</p>');
+		}
+		
+		$password = $_POST['password'];
+		
+		$check = mysqli_query($DB, "SELECT * FROM users WHERE username = '".$_POST['username']."'")or die(mysqli_error($DB));
+		
+ 		//Gives error if user already exist
+ 		$check2 = mysqli_num_rows($check);
+		if ($check2 == 0) {
+			die("<p>Sorry, user name does not exisits.</p>");
+		}
+		else
+		{
+			while($info = mysqli_fetch_array($check )) 	{
+			 	//gives error if the password is wrong
+				if ($password != $info['pass']) {
+					die('Incorrect password, please try again.');
+				}
+			}
+			$hour = time() + 3600; 
+			setcookie('hackme', $_POST['username'], $hour); 
+			setcookie('hackme_pass', $password, $hour);
+			header("Location: members.php");
+		}
+	}
+		?>  
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>hackme</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<?php
+	include('header.php');
+?>
+<div class="post">
+	<div class="post-bgtop">
+		<div class="post-bgbtm">
+        <h2 class = "title">hackme bulletin board</h2>
+        	<?php
+            if(!isset($_COOKIE['hackme'])){
+				 die('Why are you not logged in?!');
+			}else
+			{
+				print("<p>Logged in as <a>$_COOKIE[hackme]</a></p>");
+			}
+			?>
+        </div>
+    </div>
+</div>
+
+<?php
+	$threads = mysqli_query($DB, "SELECT * FROM threads ORDER BY date DESC")or die(mysqli_error($DB));
+	while($thisthread = mysqli_fetch_array( $threads )){
+?>
+	<div class="post">
+	<div class="post-bgtop">
+	<div class="post-bgbtm">
+		<h2 class="title"><a href="show.php?pid=<?php echo $thisthread['id'] ?>"><?php echo $thisthread['title']?></a></h2>
+							<p class="meta"><span class="date"> <?php echo date('l, d F, Y',$thisthread['date']) ?> - Posted by <a href="#"><?php echo $thisthread['username'] ?> </a></p>
+
+	</div>
+	</div>
+	</div> 
+
+<?php
+}
+	include('footer.php');
+?>
+</body>
+</html>
---
+++ course/cos232/project/web/src/register.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>hackme</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<?php
+	include('connect.php');
+	include('header.php');
+	$DB = connect();
+?>
+<div class="post">
+	<div class="post-bgtop">
+		<div class="post-bgbtm">
+        <h2 class = "title">hackme Registration</h2>
+        <?php
+		//if the registration form is submitted 
+		if (isset($_POST['submit'])) {
+			
+			$_POST['uname'] = trim($_POST['uname']);
+			if(!$_POST['uname'] | !$_POST['password'] |
+				!$_POST['fname'] | !$_POST['lname']) {
+ 				die('<p>You did not fill in a required field.
+				Please go back and try again!</p>');
+ 			}
+			
+			$password = $_POST['password'];
+			
+			$check = mysqli_query($DB, "SELECT * FROM users WHERE username = '".$_POST['uname']."'") or die(mysqli_error($DB));
+ 
+ 		//Gives error if user already exist
+ 		$check2 = mysqli_num_rows($check);
+		if ($check2 != 0) {
+			die('<p>Sorry, user name already exisits.</p>');
+		}
+		else
+		{
+			mysqli_query($DB, "INSERT INTO users (username, pass, fname, lname) VALUES ('".$_POST['uname']."', '". $password ."', '". $_POST['fname']."', '". $_POST['lname'] ."');")or die(mysqli_error());
+			
+			echo "<h3> Registration Successful!</h3> <p>Welcome ". $_POST['fname'] ."! Please log in...</p>";
+		} 
+        ?>    
+        <?php
+		}else{
+        ?>
+        	<form  method="post" action="register.php">
+            <table>
+                <tr>
+                    <td> Username </td> 
+                    <td> <input type="text" name="uname" maxlength="20"/> </td>
+                    <td> <em>choose a login id</em> </td>
+                </tr>
+                <tr>
+                    <td> Password </td>
+                    <td> <input type="password" name="password" maxlength="40" /> </td>
+                </tr>
+                <tr>
+                    <td> First Name </td>
+                    <td> <input type="text" name="fname" maxlength="25"/> </td>
+                </tr>
+                 <tr>
+                    <td> Last Name </td>
+                    <td> <input type="text" name="lname" maxlength="25"/> </td>
+                </tr>
+                <tr>
+                    <td> <input type="submit" name="submit" value="Register" /> </td>
+                </tr>
+            </table>
+            </form>
+        <?php
+		}
+		?>
+        </div>
+    </div>
+</div>
+<?php
+	include('footer.php');
+?>
+</body>
+</html>
