44993358358ea14269c39d0ce124ceb2433b8f5b
nflasher <nate_flasher@taylor.edu>
2020-10-24 17:17:29 -0400

    Made some more progress. Specifically, I now have the offsets to the first attribute (header) for each MFT calculated. I moved on to creating a byte buffer for each attribute header. When I work on this next I plan to parse the attribute header and grab the offsets to soon parse the file attributes themselves. I feel quite accomplished with the work I've done today. Went through a lot of struggles getting this current code to work. The big comment section is something I tried at first, but has fallen through and  I'm currently using it as a resource I can use to what I can do or what I may not want to do. The comment section will be deleted before the end result of the project is done.

--- course/cos411/project/ntfs/NTFSDump.java
+++ course/cos411/project/ntfs/NTFSDump.java
@@ -66,24 +66,53 @@ public class NTFSDump {
         }
 
  */
+        //creates file input stream for the disk image
         InputStream inputStream = new FileInputStream(diskImage);
+
+        //skips (byteOffsett)bytes through the disk image to the MFT
         inputStream.skip(byteOffset);
+
+        //loops through each MFT entry
         for (int i = 0; i < allMFTEntries.length; i++){
+
+            //reads each mft entry one by one
             inputStream.read(allMFTEntries[i], 0, 1024);
-            ByteBuffer tempBuf = ByteBuffer.wrap(allMFTEntries[i]);
-            tempBuf.order(ByteOrder.LITTLE_ENDIAN);
-            int y = tempBuf.getInt(24);
-            int x = tempBuf.getInt(28);
-            int offsetToFirstAttr = tempBuf.getShort(20);
-            System.out.println("Offset to first attribute for entry " + Arrays.toString(allMFTEntries[i]) + " =  " + offsetToFirstAttr + "\n");
-            //System.out.println("Used space of file: " + y + "bytes " + "\n");
-            //System.out.println("Allocated size of file: " + x + "bytes " + "\n");
+
+            //wraps each MFT entry into a byte buffer
+            ByteBuffer forFirstAttributeBuffer = ByteBuffer.wrap(allMFTEntries[i]);
+
+            //sets the byte order of the byte buffer to little endian
+            forFirstAttributeBuffer.order(ByteOrder.LITTLE_ENDIAN);
+
+            //gets the offset to each MFT entry's first attribute and stores in in a variable
+            int offsetToFirstAttr = forFirstAttributeBuffer.getShort(20);
+            //System.out.println(offsetToFirstAttr + "\n");
+
+           //creates a new byte array for the attribute header of size 16
+            byte [] attributeHeader = new byte [16];
+
+            //loops this entry's attribute header and sets each attribute header's index to the correct offset in it's current MFT entry
+            for(int a = 0; a < attributeHeader.length; a++){
+                attributeHeader[a] = allMFTEntries[i][offsetToFirstAttr + a];
+            }
+
+            //wraps the attribute header in a byte buffer
+            ByteBuffer attributeHeaderBuffer = ByteBuffer.wrap(attributeHeader);
+
+            //sets the byte buffer for the attribute header to use little endian format for bytes
+            attributeHeaderBuffer.order(ByteOrder.LITTLE_ENDIAN);
+
+
+
+
         }
 
-
+        //closes file stream
         inputStream.close();
 
     }
+
+
 
     public static void main(String[] args) throws IOException {
 
@@ -103,4 +132,3 @@ public class NTFSDump {
 
 
 }
-
