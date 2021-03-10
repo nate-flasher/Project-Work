bd5c8099d902465783aa6324c6577c631cc0d3b5
Nate Flasher <nate_flasher@taylor.edu>
2021-03-10 13:19:21 -0500

    Created prj files

---
+++ course/cos331/project/HTTPServer/cmake-build-debug/CMakeFiles/HTTPServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HTTPServer.dir/listener.c.o"
+  "HTTPServer"
+  "HTTPServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HTTPServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
