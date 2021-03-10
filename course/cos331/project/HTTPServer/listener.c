a5cdae967650e60bc125bc3cbf206689e9866268
Nate Flasher <nate_flasher@taylor.edu>
2021-03-10 14:02:46 -0500

    Code from class to request a socket with descripters from operating system

--- course/cos331/project/HTTPServer/listener.c
+++ course/cos331/project/HTTPServer/listener.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <memory.h>
+#include <string.h>
+
+#define MYHOST "localhost"
+#define MYPORT "8080"
 
 int main() {
-    printf("Hello, World!\n");
+
+    struct addrinfo hints;
+    struct addrinfo * address_resource;
+    int socket_descriptor;
+    int return_value;
+
+    memset(&hints, 0, sizeof(hints)); //clears out any data in hints struct before we write to it
+
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP connection, not UDP
+    hints.ai_flags = AI_PASSIVE; // Specifies socket to listen
+
+    return_value = getaddrinfo(MYHOST, MYPORT, &hints, &address_resource);
+
+    if(return_value != 0){
+        printf("Error: getaddrinfo failed");
+        return 1;
+    }
+
+    socket_descriptor = socket(
+            address_resource->ai_family, //IPv4
+            address_resource->ai_socktype, // Streaming Protocol
+            address_resource->ai_protocol //TCP
+            );
+
     return 0;
 }
