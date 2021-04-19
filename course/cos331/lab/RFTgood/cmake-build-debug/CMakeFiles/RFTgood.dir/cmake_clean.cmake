7ce3426d3ccc7c4a9d435c115e4566a0cd6bced9
Nate Flasher <nate_flasher@taylor.edu>
2021-04-19 13:51:24 -0400

    Implemented UDP checksum portion

---
+++ course/cos331/lab/RFTgood/cmake-build-debug/CMakeFiles/RFTgood.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RFTgood.dir/main.c.o"
+  "RFTgood"
+  "RFTgood.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RFTgood.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- course/cos331/lab/RFTgood/main.c
+++ course/cos331/lab/RFTgood/main.c
@@ -4,6 +4,7 @@
 #include <netinet/udp.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
 
 
 #define IP_SRC "127.0.0.1"
@@ -11,11 +12,50 @@
 #define UDP_SRC_PORT 12345
 #define UDP_SRC_DEST 54321
 
+
+struct pseudo_ip_header {
+    struct  in_addr ip_src;
+    struct  in_addr ip_dst;
+    u_char  unused; // set to zero
+    u_char  ip_p;
+    u_short uh_ulen;
+};
+
+unsigned short checksum1(const char *buf, unsigned size)
+{
+    unsigned sum = 0;
+    int i;
+
+    /* Accumulate checksum */
+    for (i = 0; i < size - 1; i += 2)
+    {
+        unsigned short word16 = *(unsigned short *) &buf[i];
+        sum += word16;
+    }
+
+    /* Handle odd-sized case */
+    if (size & 1)
+    {
+        unsigned short word16 = (unsigned char) buf[i];
+        sum += word16;
+    }
+
+    /* Fold to get the ones-complement result */
+    while (sum >> 16) sum = (sum & 0xFFFF)+(sum >> 16);
+
+    /* Invert to get the negative in ones-complement arithmetic */
+    return (unsigned short) ~sum;
+}
+
 int main() {
 
     struct ip ip_header;
     struct udphdr udp_header;
+    struct pseudo_ip_header faked_ip_header_for_checksum;
+    char * fake_dgram_for_udp_checksum;
     char * data = "Hello World!";
+    short total_fake_dgram_size = sizeof(faked_ip_header_for_checksum) + sizeof(udp_header) + strlen(data);
+
 
     struct in_addr from_ip;
     struct in_addr to_ip;
@@ -37,7 +77,7 @@ int main() {
     ip_header.ip_off = 0; //fragmentation offset
     ip_header.ip_ttl = 255; //how long packet is in network before it gets thrown away, each time packet is sent between routers, value is decremented
     ip_header.ip_p = IPPROTO_UDP; //sets protocol to UDP
-    ip_header.ip_sum = 0; //header checksum (always set to 0, operating system takes care of it for us)
+    ip_header.ip_sum = 0; //header checksum (always set to 0, operating system takes care of it for us, always filled in by Linux kernel)
 
 
     from_ip.s_addr = inet_addr(IP_SRC);
@@ -45,6 +85,35 @@ int main() {
 
     to_ip.s_addr = inet_addr(IP_DST);
     ip_header.ip_dst = to_ip; //sets destination ip address
+
+
+    udp_header.uh_sport = htons(UDP_SRC_PORT); //sets udp header source port to our source port global variable
+    udp_header.uh_dport = htons(UDP_SRC_DEST); //sets udp header destination port to our destination port global variable
+    udp_header.uh_ulen = htons(sizeof(udp_header) + strlen(data)); //sets udp header length
+    udp_header.uh_sum = 0; //checksum field set to 0
+
+
+    //need to fill in new (fake) ip header values to use for checksum
+    faked_ip_header_for_checksum.ip_src = ip_header.ip_src;
+    faked_ip_header_for_checksum.ip_dst = ip_header.ip_dst;
+    faked_ip_header_for_checksum.unused = 0;
+    faked_ip_header_for_checksum.ip_p = ip_header.ip_p;
+    faked_ip_header_for_checksum.uh_ulen = udp_header.uh_ulen;
+
+    fake_dgram_for_udp_checksum = malloc(total_fake_dgram_size); //creating memory on heap
+    memset(fake_dgram_for_udp_checksum, 0, total_fake_dgram_size); // zeroes out memory in that buffer
+
+    char * cursor = fake_dgram_for_udp_checksum;
+    memcpy(cursor, &faked_ip_header_for_checksum, sizeof(faked_ip_header_for_checksum)); //puts faked ip header into beginning of buffer
+    cursor += sizeof(faked_ip_header_for_checksum); //updates cursor
+
+    memcpy(cursor, &udp_header, sizeof(udp_header)); //puts udp header into next part of buffer
+    cursor += sizeof(udp_header); //updates cursor
+
+    memcpy(cursor, &data, strlen(data)); //puts our data into end of buffer...we now have our buffer filled and is rdy to generate our udp checksum!
+
+    udp_header.uh_sum = checksum1(fake_dgram_for_udp_checksum, total_fake_dgram_size); //creates udp checksum
+    free(fake_dgram_for_udp_checksum);
 
 
     return 0;
