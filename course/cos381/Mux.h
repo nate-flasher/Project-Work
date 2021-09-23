8296abdad772d0ed43337095abea8b386b2cf4f4
nflasher <nate_flasher@taylor.edu>
2021-09-23 17:21:20 -0400

    Final update, added a call to update listeners to fix the failing tests.

--- course/cos381/Mux.h
+++ course/cos381/Mux.h
@@ -3,49 +3,53 @@
 #include "common.h"
 
 constexpr int log2(int n) {
-     int i = 0;
-     while (n > 1) {
-         n /= 2;
-         ++i;
-         }
+    int i = 0;
+    while (n > 1) {
+        n /= 2;
+        ++i;
+    }
 
-     return i;
-     }
+    return i;
+}
 
- template <int size, int width>
- class Mux : public LogicUnit<width>, public Listener<log2(size)> {
-     // private data members necessary to keep track of inputs
-     // private data members necessary to keep track of select bits
-     Value<log2(size)> select_bits_value;
-     Value<width> returned_value;
-     Value<width> input_port_array [size];
+template <int size, int width>
+class Mux : public LogicUnit<width>, public Listener<log2(size)> {
+    // private data members necessary to keep track of inputs
+    // private data members necessary to keep track of select bits
+    Value<log2(size)> select_bits_value;
+    Value<width> returned_value;
+    Value<width> input_port_array [size];
 
-     public:
-     Mux(const std::string& name = "");
-     void update_input(int input_port, Value<width> value) override;
-     void update_input(int input_port, Value<log2(size)> value) override;
-     Value<width> get_output() const override;
-     };
+public:
+    Mux(const std::string& name = "");
+    void update_input(int input_port, Value<width> value) override;
+    void update_input(int input_port, Value<log2(size)> value) override;
+    Value<width> get_output() const override;
+};
 
 template<int size, int width>
 Mux<size, width>::Mux(const std::string &name) {
 
 }
 
- template <int size, int width>
- void Mux<size, width>::update_input(int input_port, Value<width> value) {
-     // code to handle input updates here
-     this->input_port_array[input_port] = value;
-     }
+template <int size, int width>
+void Mux<size, width>::update_input(int input_port, Value<width> value) {
+    // code to handle input updates here
+    this->input_port_array[input_port] = value;
 
-     //for our select bits
- template <int size, int width>
- void Mux<size, width>::update_input(int input_port, Value<log2(size)> value) {
-     // code to handle select updates here
-     this->select_bits_value = value;
-     }
+    this->update_listeners();
+}
 
- template <int size, int width>
- Value<width> Mux<size, width>::get_output() const {
-     return Value<width> (this->input_port_array[this->select_bits_value]);
-     }
+//for our select bits
+template <int size, int width>
+void Mux<size, width>::update_input(int input_port, Value<log2(size)> value) {
+    // code to handle select updates here
+    this->select_bits_value = value;
+
+    this->update_listeners();
+}
+
+template <int size, int width>
+Value<width> Mux<size, width>::get_output() const {
+    return Value<width> (this->input_port_array[this->select_bits_value]);
+}
